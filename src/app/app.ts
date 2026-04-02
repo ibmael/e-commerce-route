88475ee189463c25e21e22ab184dbffc73163b47
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-04-02 20:33:02 +0200

    INSTALLING THE PACKAGES

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
+import { FlowbiteService } from './core/services/flowbite-servives';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }

1376b9c5525e668688dde82d7b78fc70fe2d657c
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-04-11 17:50:42 +0200

    feat: add validation messages for registration form fields

--- src/app/features/register/register.ts
+++ src/app/features/register/register.ts
@@ -1,6 +1,12 @@
 import { register } from 'swiper/element/bundle';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,19 +17,22 @@ import { RouterLink } from '@angular/router';
 })
 export class Register {
   private readonly formBuilder = inject(FormBuilder);
-  registerForm: FormGroup = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+  registerForm: FormGroup = this.formBuilder.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+        ],
       ],
-    ],
-    rePassword: ['', [Validators.required]],
-    phone: ['', [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]],
-  });
+      rePassword: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]],
+    },
+    { validators: [this.confirmPassword] },
+  );
 
   submitForm(): void {
     if (this.registerForm.valid) {
@@ -31,5 +40,16 @@ export class Register {
     } else {
       console.log('Form is invalid');
     }
+  }
+  confirmPassword(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const rePassword = group.get('rePassword')?.value;
+
+    if (rePassword !== password && rePassword !== '') {
+      group.get('rePassword')?.setErrors({ mismatch: true });
+
+      return { mismatch: true };
+    }
+    return null;
   }
 }

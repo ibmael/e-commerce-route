ae8830b5498bfc3960bf83d08fc54340fabb2f39
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-04-14 15:35:35 +0200

    finshing the not found page

--- src/app/features/not-found/not-found.ts
+++ src/app/features/not-found/not-found.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {}
+export class NotFound {
+  private readonly location = inject(Location);
+
+  goBack(): void {
+    this.location.back();
+  }
+}
